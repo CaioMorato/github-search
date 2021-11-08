c2ca594218b4b958e8e7d41291833aefb95cc9de
Caio Morato <morato_dev@protonmail.com>
2021-11-07 21:30:35 -0300

    tentativa ajuste deploy

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -30,8 +30,9 @@ function SearchBar() {
         </div>
       </div>
       <div className="flex-none">
-        <button className="btn btn-square btn-ghost" onClick={() => handleClick()}>
-          <svg
+        <button className="btn btn-square btn-ghost" type="button" onClick={() => handleClick()}>
+          Search
+          {/* <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
@@ -45,7 +46,7 @@ function SearchBar() {
               strokeWidth="2"
               d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
             ></path>
-          </svg>
+          </svg> */}
         </button>
       </div>
     </div>
