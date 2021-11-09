eae59642a5ca54a963a1c3d9559af14df1079edf
Caio Morato <morato_dev@protonmail.com>
2021-11-09 09:23:55 -0300

    mudando identidade do site

--- src/components/NoUser.js
+++ src/components/NoUser.js
@@ -8,7 +8,9 @@ function NoUser() {
   const { darkTheme } = useContext(MyContext);
   return (
     <section className="text-center my-12">
-      <h3 className={`${themeToggleText(darkTheme)} logo font-bold`}>Digite um nome de usuário</h3>
+      <h3 className={`${themeToggleText(darkTheme)} logo font-bold`}>
+        Use the search bar to find a username.
+      </h3>
     </section>
   );
 }
